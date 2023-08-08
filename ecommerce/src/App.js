ad0d514fadbc3eda296216d28525563ee2bad537
Pavlova <alja644@gmail.com>
2023-08-08 10:30:46 +0300

    Minor Change

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MobilePhones from "./pages/Products_1/Products_1";
-import SmartWatches from "./pages/Products_2/Products_2";
+import Products_1 from "./pages/Products_1/Products_1";
+import Products_2 from "./pages/Products_2/Products_2";
 
 const router = createBrowserRouter([
-  { path: "/mobile-phones", element: <MobilePhones /> },
-  { path: "/smart-watches", element: <SmartWatches /> },
+  { path: "/", element: <Products_1 /> },
+  { path: "/products_2", element: <Products_2 /> },
 ]);
 
 function App() {
